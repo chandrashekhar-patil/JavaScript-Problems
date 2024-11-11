6af25ca3fadf7c7cb9bb85ebceeab8cd8fd49427
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2024-11-11 18:55:52 +0530

    Update fibonacci.js

--- problems/fibonacci.js
+++ problems/fibonacci.js
@@ -9,14 +9,25 @@
 // let result = fibonacci(5);
 // console.log(result);
 
-function fibonacci(num) {
-  if (num === 0) {
-    return 0;
-  } else if (num === 1) {
-    return 1;
-  } else {
-    return fibonacci(num - 1) + fibonacci(num - 2);
-  }
+// function fibonacci(num) {
+//   if (num === 0) {
+//     return 0;
+//   } else if (num === 1) {
+//     return 1;
+//   } else {
+//     return fibonacci(num - 1) + fibonacci(num - 2);
+//   }
+// }
+// let result = fibonacci(20);
+// console.log(result);
+
+let num = 5;
+firstNo = 0;
+secondNo = 1;
+for (i = 0; i < num; i++) {
+  console.log(firstNo);
+  thirdNo = firstNo + secondNo;
+  firstNo = secondNo;
+  secondNo = thirdNo;
 }
-let result = fibonacci(20);
-console.log(result);
+
